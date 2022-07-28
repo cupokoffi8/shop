1fb55ddae378ae1692ceabcc013e2304521703d7
Alex <cupokoffi@gmail.com>
2022-07-28 07:00:00 -0400

    Deployment gas price update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,27 @@ const hre = require('hardhat');
 
 async function main() {
   const NFTMarketplace = await hre.ethers.getContractFactory('NFTMarketplace');
+
+  const gasPrice = await NFTMarketplace.signer.getGasPrice();
+  console.log(`Current gas price: ${gasPrice}`);
+
+  const estimatedGas = await NFTMarketplace.signer.estimateGas(
+    NFTMarketplace.getDeployTransaction(),
+  );
+  console.log(`Estimated gas: ${estimatedGas}`);
+
+  const deploymentPrice = gasPrice.mul(estimatedGas);
+  const deployerBalance = await NFTMarketplace.signer.getBalance();
+  console.log(`Deployer balance:  ${ethers.utils.formatEther(deployerBalance)}`);
+  console.log(`Deployment price:  ${ethers.utils.formatEther(deploymentPrice)}`);
+  if (deployerBalance.lt(deploymentPrice)) {
+    throw new Error(
+      `Insufficient funds. Top up your account balance by ${ethers.utils.formatEther(
+        deploymentPrice.sub(deployerBalance),
+      )}`,
+    );
+  }
+
   const nftMarketplace = await NFTMarketplace.deploy();
 
   await nftMarketplace.deployed();
